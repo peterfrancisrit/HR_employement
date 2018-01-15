191c991ed1b59f341caee66069bb70a514013ae0
Harry Ritchie <harryritchie@client0084.vpn.uni-saarland.de>
2018-01-15 01:22:34 +0100

    with AUC

--- yummy.py
+++ yummy.py
@@ -1,9 +1,10 @@
+# Author: Harry Ritchie <hritchie8175@gmail.com>
 # Application: HR employement data
 #############################
 
 # Build a prediction tool from linear regression to predict satisfaction level based on average monthly work hours 
 # and last evaluation. I then built a classification tool on whether or not employees left their jobs based on 
-# last evaluation and average monthly hours. 
+# last evaluation, average monthly hours and satisfaction level.
 
 # REGRESSION 
 
@@ -13,7 +14,7 @@ import numpy as np
 
 # Import data using pandas
 import pandas as pd
-data = pd.read_csv('/Users/harryritchie/Documents/Personal/Employment/HR_comma_sep.csv',delimiter=';',header=0,engine='python')
+data = pd.read_csv('/Users/harryritchie/Documents/Personal/Employment/HR_employement/HR_comma_sep.csv',delimiter=';',header=0,engine='python')
 
 # Separate response from predictors
 y = data['satisfaction_level']
@@ -45,5 +46,121 @@ print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
 print('Variance score: %.2f' % r2_score(y_test, y_pred))
 
 # CLASSIFICATION 
+
+# Courtesy of Author: Tim Head <betatim@gmail.com>
+np.random.seed(10)
+from sklearn.datasets import make_classification
+from sklearn.ensemble import (RandomTreesEmbedding, RandomForestClassifier,
+                              GradientBoostingClassifier)
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import roc_curve
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, roc_auc_score
+
+n_estimator = 2
+depth = 6
+y = data['left']
+X = data[['last_evaluation', 'average_montly_hours','satisfaction_level']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+# It is important to train the ensemble of trees on a different subset
+# of the training data than the linear regression model to avoid
+# overfitting, in particular if the total number of leaves is
+# similar to the number of training samples
+X_train, X_train_lr, y_train, y_train_lr = train_test_split(X_train,
+                                                            y_train,
+                                                            test_size=0.5)
+
+# Unsupervised transformation based on totally random trees
+rt = RandomTreesEmbedding(max_depth=depth, n_estimators=n_estimator,
+    random_state=0)
+
+rt_lm = LogisticRegression()
+pipeline = make_pipeline(rt, rt_lm)
+pipeline.fit(X_train, y_train)
+y_pred_rt = pipeline.predict_proba(X_test)[:, 1]
+fpr_rt_lm, tpr_rt_lm, _ = roc_curve(y_test, y_pred_rt)
+auc_rt_lm = roc_auc_score(y_test, y_pred_rt)
+
+# Supervised transformation based on random forests
+rf = RandomForestClassifier(max_depth=depth, n_estimators=n_estimator)
+rf_enc = OneHotEncoder()
+rf_lm = LogisticRegression()
+rf.fit(X_train, y_train)
+rf_enc.fit(rf.apply(X_train))
+rf_lm.fit(rf_enc.transform(rf.apply(X_train_lr)), y_train_lr)
+
+y_pred_rf_lm = rf_lm.predict_proba(rf_enc.transform(rf.apply(X_test)))[:, 1]
+fpr_rf_lm, tpr_rf_lm, _ = roc_curve(y_test, y_pred_rf_lm)
+auc_rf_lm = roc_auc_score(y_test, y_pred_rf_lm)
+
+grd = GradientBoostingClassifier(n_estimators=n_estimator)
+grd_enc = OneHotEncoder()
+grd_lm = LogisticRegression()
+grd.fit(X_train, y_train)
+grd_enc.fit(grd.apply(X_train)[:, :, 0])
+grd_lm.fit(grd_enc.transform(grd.apply(X_train_lr)[:, :, 0]), y_train_lr)
+
+y_pred_grd_lm = grd_lm.predict_proba(
+    grd_enc.transform(grd.apply(X_test)[:, :, 0]))[:, 1]
+fpr_grd_lm, tpr_grd_lm, _ = roc_curve(y_test, y_pred_grd_lm)
+auc_grd_lm = roc_auc_score(y_test, y_pred_grd_lm)
+
+
+# The gradient boosted model by itself
+y_pred_grd = grd.predict_proba(X_test)[:, 1]
+fpr_grd, tpr_grd, _ = roc_curve(y_test, y_pred_grd)
+auc_grd = roc_auc_score(y_test, y_pred_grd)
+
+
+# The random forest model by itself
+y_pred_rf = rf.predict_proba(X_test)[:, 1]
+fpr_rf, tpr_rf, _ = roc_curve(y_test, y_pred_rf)
+auc_rf = roc_auc_score(y_test, y_pred_rf)
+
+# Print AUC ROC values
+print('\n')
+print("AUC VALUES",'\n')
+print("N ESTIMATORS = ", n_estimator, "MAX TREE DEPTH = ", depth,'\n')
+print("RANDOM TREES AUC = ", auc_rt_lm, '\n')
+print("RANDOM FOREST + LINEAR MODEL AUC = ", auc_rf_lm, '\n')
+print("GRADIENT BOOSTER + LINEAR MODEL AUC = ", auc_grd_lm, '\n')
+print("GRADIENT BOOSTED MODEL AUC = ", auc_grd, '\n')
+print("RANDOM FOREST AUC = ", auc_rf, '\n')
+
+
+plt.figure(1)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr_rt_lm, tpr_rt_lm, label='RT + LR')
+plt.plot(fpr_rf, tpr_rf, label='RF')
+plt.plot(fpr_rf_lm, tpr_rf_lm, label='RF + LR')
+plt.plot(fpr_grd, tpr_grd, label='GBT')
+plt.plot(fpr_grd_lm, tpr_grd_lm, label='GBT + LR')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend(loc='best')
+plt.show()
+
+plt.figure(2)
+plt.xlim(0, 0.2)
+plt.ylim(0.8, 1)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot(fpr_rt_lm, tpr_rt_lm, label='RT + LR')
+plt.plot(fpr_rf, tpr_rf, label='RF')
+plt.plot(fpr_rf_lm, tpr_rf_lm, label='RF + LR')
+plt.plot(fpr_grd, tpr_grd, label='GBT')
+plt.plot(fpr_grd_lm, tpr_grd_lm, label='GBT + LR')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve (zoomed in at top left)')
+plt.legend(loc='best')
+plt.show()
+
+
+
+
+
 
 
